b197f00286123c23036e91118e7aa935b4d43244
zb <8761804@iesplayamar.es>
2025-07-17 05:18:22 -0300

    da

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,1 @@
 import 'zone.js';
-
-// Opcionalmente:
-import 'core-js/es/array';
-import 'core-js/es/object';
-import 'core-js/es/promise';
-import 'core-js/es/symbol';
-import 'core-js/es/reflect'
