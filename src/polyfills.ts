5e98afd1ad44147a5e35b289184ca6c6c349e478
zb <8761804@iesplayamar.es>
2025-07-18 17:18:52 -0300

    poly

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,1 @@
 import 'zone.js';  // Angular necesita esto
-import 'core-js/es/array';
-import 'core-js/es/object';
-import 'core-js/es/promise';
-import 'core-js/es/string';
-import 'core-js/es/symbol';
